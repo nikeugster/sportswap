37067d8f7c7f6c724c319fd37bc4ece39fff9719
Nik Eugster <mail@nikeugster.ch>
2021-06-03 11:19:15 +0200

    fix search

--- app/models/offer.rb
+++ app/models/offer.rb
@@ -5,8 +5,8 @@ class Offer < ApplicationRecord
   has_many_attached :photos
   validates :category, :condition, :title, :description, :street, :zip_code, :city, :country, :compensation_type, :compensation_value, presence: true
   include PgSearch::Model
-  pg_search_scope :search_by_title_and_city,
-    against: [:title, :description, :city],
+  pg_search_scope :search_by_title_and_location,
+    against: [:title, :description, :city, :street, :zip_code],
     using: {
       tsearch: { prefix: true }
     }
