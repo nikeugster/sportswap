290943994ce25b2ec5aa7214fc1a19729a6c3d44
Benedikt Loeffler <benedikt.loe@hotmail.com>
2021-06-07 18:28:12 +0200

    Algolia styling

--- app/javascript/plugins/init_autocomplete.js
+++ app/javascript/plugins/init_autocomplete.js
@@ -1,9 +1,13 @@
 import places from 'places.js';
 
 const initAutocomplete = () => {
+  const reconfigurableOptions = {
+    language: 'en'
+  };
+
   const addressInput = document.querySelector('.last-input');
   if (addressInput) {
-    places({ container: addressInput });
+    places({ container: addressInput }).configure(reconfigurableOptions);
   }
 };
 
