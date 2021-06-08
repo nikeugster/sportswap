78b3a08b86f3193c54c87de10a11eb7f9eafa21f
Benedikt Loeffler <benedikt.loe@hotmail.com>
2021-06-08 12:40:47 +0200

    Add review button to booking

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,8 +1,25 @@
 class ReviewsController < ApplicationController
+  before_action :set_restaurant, only: [:new, :create]
+
+  def new
+    @review = Review.new
+  end
+
   def create
+    @review = Review.new(review_params)
+    @review.offer = @offer
+    if @review.save
+      redirect_to offer_path(@offer)
+    else
+      render :new
+    end
   end
 
   private
+
+  def set_offer
+    @offer = Offer.find(params[:offer_id])
+  end
 
   def review_params
     params.require(:review).permit(:rating, :content)
--- app/models/review.rb
+++ app/models/review.rb
@@ -3,4 +3,6 @@ class Review < ApplicationRecord
   belongs_to :user
 
   validates :content, :rating, presence: true
+  validates :rating, inclusion: { in: 0..5 }
+  validates :rating, numericality: { only_integer: true }
 end
