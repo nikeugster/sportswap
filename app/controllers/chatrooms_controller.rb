5cd1b5914e140868d16d11b99a0c1da6e4c5880d
Dominik Dewitz <dewitzdominik@kabelmail.de>
2021-06-08 14:34:38 +0200

    remove redundant code

--- app/controllers/chatrooms_controller.rb
+++ app/controllers/chatrooms_controller.rb
@@ -1,11 +1,4 @@
 class ChatroomsController < ApplicationController
-  def create
-    @chatroom = Chatroom.new
-    @chatroom.sender = current_user
-    @chatroom.recipient = @offer.user
-    @chatroom.save
-  end
-
   def show
     @chatroom = Chatroom.find(params[:id])
     @message = Message.new
