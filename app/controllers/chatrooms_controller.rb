284ae41dc1f7f2d3a0582dea89c120089881811e
Dominik Dewitz <dewitzdominik@kabelmail.de>
2021-06-08 14:30:57 +0200

    Add chatroom creation on booking - create chatroom only if no chatroom between the two users exists

--- app/controllers/chatrooms_controller.rb
+++ app/controllers/chatrooms_controller.rb
@@ -1,6 +1,13 @@
 class ChatroomsController < ApplicationController
+  def create
+    @chatroom = Chatroom.new
+    @chatroom.sender = current_user
+    @chatroom.recipient = @offer.user
+    @chatroom.save
+  end
+
   def show
     @chatroom = Chatroom.find(params[:id])
-    @message = Message.new()
+    @message = Message.new
   end
 end
