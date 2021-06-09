e5e0a4f89c37484cd678b0934c9730ba559160d5
Dominik Dewitz <dewitzdominik@kabelmail.de>
2021-06-09 15:55:09 +0200

    Add liveupdates to bookings

--- app/javascript/channels/status_update_channel.js
+++ app/javascript/channels/status_update_channel.js
@@ -1,11 +1,24 @@
 import consumer from './consumer';
 
-consumer.subscriptions.create(
-  'StatusUpdateChannel',
-  {
-    received(data) {
-      console.log(data)
-      console.log(data.booking_status)
+const initStatusUpdateChannel = () => {
+  consumer.subscriptions.create(
+    'StatusUpdateChannel',
+    {
+      received(data) {
+        console.log(data)
+        console.log(data.booking_status)
+        console.log(data.booking_id)
+
+        const bookingStatus = document.getElementById(`booking-status-${data.booking_id}`)
+
+        if (data.booking_status == "accepted") {
+          bookingStatus.innerHTML = "<p class='dashboard-accepted-text'><i class='fas fa-check-circle'></i> accepted!</p>"
+        } else if (data.booking_status == "declined") {
+          bookingStatus.innerHTML = "<p class='dashboard-declined-text'><i class='fas fa-times-circle'></i> declined!</p>"
+        }
+      },
     },
-  },
-);
+  );
+};
+
+export default initStatusUpdateChannel;
