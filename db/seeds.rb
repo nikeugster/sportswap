6ddd07ab5495bb3c0145ac0105cd1f2ffb1fb0c4
Benedikt Loeffler <benedikt.loe@hotmail.com>
2021-06-10 15:42:33 +0200

    Change user review texts final

--- db/seeds.rb
+++ db/seeds.rb
@@ -466,7 +466,7 @@ puts "Created review 1 for '#{chrissie_offer_3.title}'"
 
 chrissie_offer_3_review_2 = Review.new(
   rating: 5,
-  content: "Why does this skateboard not have any wheels??! I tried to skate in the park but it just wouldn't work??! People looked at me like I was a noob, but it's not my fault??!",
+  content: "Why does this skateboard not have any wheels??! I tried to skate in the park but it just wouldn't work??! People looked at me like I was an idiot, but it's not my fault??!",
   user_id: chrissie.id,
   offer_id: 3,
   author_id: 3
